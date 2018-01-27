e739a1f1e6ef55ec3345e482ac9e4d0bef63f947
Adrian Bigland <a.d.bigland@gmail.com>
2018-01-27 20:16:05 +0000

    Initial Commit

---
+++ NIXSwift/NIXSwift.h
@@ -0,0 +1,19 @@
+//
+//  NIXSwift.h
+//  NIXSwift
+//
+//  Created by Adrian Bigland on 27/01/2018.
+//  Copyright © 2018 Adrian Bigland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NIXSwift.
+FOUNDATION_EXPORT double NIXSwiftVersionNumber;
+
+//! Project version string for NIXSwift.
+FOUNDATION_EXPORT const unsigned char NIXSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NIXSwift/PublicHeader.h>
+
+
